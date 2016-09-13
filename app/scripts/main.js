e4ae34ca9458bfc2188273428491b3a2d7139520
emazurek <Edward.R.Mazurek@gmail.com>
2016-09-13 17:38:29 -0400

    BpmTimestamp is object, add sidebar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,52 +3,86 @@ var howManyBeatsOut = 300;
 var timeSignature = 4;
 var isMeasure;
 
-var stringTimestampToArray = function(str) {
-  // 00:01:03:29 to [0,1,3,29]
-  return(str.split(':').map(number => +number));
+var BpmTimestamp = function(inputObj) {
+  this.inputObj = inputObj;
 }
 
-var timeArrayToFrames = function(arr) {
-  // [0,1,3,29] to 3809
-  var frames = arr[3];
-  var seconds = arr[2];
-  var minutes = arr[1];
-  var hours = arr[0];
-  return(frames + seconds * 60 + minutes * 60 * 60 + hours * 60 * 60 * 60);
-}
+BpmTimestamp.prototype = {
+  stringTimestampToArray: function(str) {
+    // 00:01:03:29 to [0,1,3,29]
+    return(str.split(':').map(number => +number));
+  },
 
-var beatToTimestamp = function(bpm, beat, timeStart = [0,0,0,0]) {
-  // convert timeStart to frames
-  timeStart = timeArrayToFrames(timeStart);
-  var frames = Math.floor(30 * 60 * beat / bpm) + timeStart;
-  var seconds = Math.floor(frames / 30);
-  var minutes = Math.floor(seconds / 60);
-  var hours = Math.floor(minutes / 60);
-  frames = frames % 30;
-  seconds = seconds % 60;
-  minutes = minutes % 60;
-  hours = hours;
-  return([hours,minutes,seconds,frames]);
-}
+  timestampArrayToText: function(timestamp) {
+    return(timestamp.map(number => ('0' + number).slice(-2)).join(':'));
+  },
 
-var timestampArrayToText = function(timestamp) {
-  return(timestamp.map(number => ('0' + number).slice(-2)).join(':'));
-}
+  timeArrayToFrames: function(arr) {
+    // [0,1,3,29] to 3809
+    var frames = arr[3];
+    var seconds = arr[2];
+    var minutes = arr[1];
+    var hours = arr[0];
+    return(frames + seconds * 60 + minutes * 60 * 60 + hours * 60 * 60 * 60);
+  },
 
-var outputList = function(bpm) {
-  var textToOutput = '', nMeasure = 0;
-  for(var i = 0; i < howManyBeatsOut; i++) {
-    isMeasure = !(i % timeSignature);
-    if(isMeasure) {
-      var measure = i / timeSignature;
-      textToOutput += 'Measure ' + measure + ' begins at ' + 
-        timestampArrayToText(beatToTimestamp(bpm, i)) + '\n';
+  beatToTimestamp: function(bpm, beat, timeStartArr = [0,0,0,0]) {
+    // convert timeStart to frames
+    var timeStart = this.timeArrayToFrames(timeStartArr);
+    var frames = Math.floor(30 * 60 * beat / bpm) + timeStart;
+    var seconds = Math.floor(frames / 30);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    frames = frames % 30;
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+    hours = hours;
+    return([hours,minutes,seconds,frames]);
+  },
+
+  makeTimestampsObj: function(bpm, howManyBeatsOut, timeSignature) {
+    var timestampsOutputObj = {measures: {}};
+    for(var i = 0; i < howManyBeatsOut; i++) {
+      isMeasure = !(i % timeSignature);
+      if(isMeasure) {
+        var measure = i / timeSignature;
+        timestampsOutputObj.measures[measure] = {timestamp: this.beatToTimestamp(bpm, i)};
+      }
     }
+    return(timestampsOutputObj);
+  },
+
+  outputListToHTML: function(timestampsOutputObj, lineBreaksArr) {
+    var output = '';
+    var lineBreaksIndex = 0;
+    var nLinesBeforeBreak = lineBreaksArr[0];
+    var breakCounter = 0;
+    for(var measure in timestampsOutputObj.measures) {
+      if(breakCounter == nLinesBeforeBreak) {
+        output += '<br/>';
+        lineBreaksIndex = lineBreaksArr[lineBreaksIndex + 1] !== undefined ? lineBreaksIndex + 1 : 0;
+        breakCounter = 0;
+        nLinesBeforeBreak = lineBreaksArr[lineBreaksIndex];
+      }
+      breakCounter++;
+      output += '<div class="measure-no">' + (+measure+1) + '</div>' + 
+        '<div class="timestamp">' + this.timestampArrayToText(timestampsOutputObj.measures[measure].timestamp) + '</div><br/>';
+    }
+    return(output);
   }
-  return(textToOutput);
 }
 
 document.getElementById('submit').addEventListener('click', function(){
-    var bpm = document.getElementById('bpm').value;
-    document.getElementById('output').innerHTML = outputList(bpm).replace(/\n/gi, '<br/>');
+  var instance = new BpmTimestamp();
+  var bpm = document.getElementById('bpm').value;
+  //var beatsPerMeasure = document.getElementById('beats-per-measure').value;
+  var lineBreaks = document.getElementById('separate-measures-array').value.split(',');
+  var timeSignature = document.getElementById('beats-per-measure').value;
+  var timestampsHTML = instance.outputListToHTML(instance.makeTimestampsObj(bpm, howManyBeatsOut, timeSignature), lineBreaks);
+  document.getElementById('output').innerHTML = timestampsHTML;
 });
+
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,28 +1,33 @@
 (function () {
   'use strict';
 
-  describe('', function () {
-    describe('#beatToTimestamp(bpm, beat)', function () {
+  describe('BpmTimestamp:', function () {
+    describe('#beatToTimestamp(bpm, beat, timeStartArr = [0,0,0,0])', function () {
+      var instance = new BpmTimestamp();
 
       it('should give [0, 0, 0, 0] for any bpm when the beat is 1', function () {
-        beatToTimestamp(bpm = 60, beat = 0).should.equal([0,0,0,0]);
-        beatToTimestamp(bpm = 123, beat = 0).should.equal([0,0,0,0]);
-        beatToTimestamp(bpm = 15, beat = 0).should.equal([0,0,0,0]);
+        instance.beatToTimestamp(60, 0).should.deep.equal([0,0,0,0]);
+        instance.beatToTimestamp(123, 0).should.deep.equal([0,0,0,0]);
+        instance.beatToTimestamp(15, 0).should.deep.equal([0,0,0,0]);
       });
 
       it('should pass these easy tests at 60bpm', function() {
-        beatToTimestamp(bpm = 60, beat = 4).should.equal([0,0,4,0]);
-        beatToTimestamp(bpm = 60, beat = 8).should.equal([0,0,8,0]);
-        beatToTimestamp(bpm = 60, beat = 60).should.equal([0,1,0,0]);
-        beatToTimestamp(bpm = 60, beat = 61).should.equal([0,1,1,0]);
+        instance.beatToTimestamp(60, 4).should.deep.equal([0,0,4,0]);
+        instance.beatToTimestamp(60, 60).should.deep.equal([0,1,0,0]);
+        instance.beatToTimestamp(60, 61).should.deep.equal([0,1,1,0]);
+        instance.beatToTimestamp(60, 3600).should.deep.equal([1,0,0,0]);
       });
 
-      it('should ', function() {
-        beatToTimestamp(bpm = 60, beat = 5).should.equal([0,0,4,0]);
-        beatToTimestamp(bpm = 60, beat = 9).should.equal([0,0,8,0]);
-        beatToTimestamp(bpm = 60, beat = 13).should.equal([0,0,12,0]);
-        beatToTimestamp(bpm = 60, beat = 61).should.equal([0,2,0,0]);
+      it('should shift the array when supplied a timeStartArr', function() {
+        instance.beatToTimestamp(60, 5).should.deep.equal([0,0,4,0]);
+        instance.beatToTimestamp(60, 9).should.deep.equal([0,0,8,0]);
+        instance.beatToTimestamp(60, 13).should.deep.equal([0,0,12,0]);
+        instance.beatToTimestamp(60, 61).should.deep.equal([0,2,0,0]);
       });      
+
+    });
+
+    describe('#stringTimestampToArray(str)', function() {
 
     });
   });
